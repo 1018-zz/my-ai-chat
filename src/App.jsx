f1fa25aec7d11830eb746701957b7b840098b5d1
姜澈 <1962489694@qq.com>
2026-08-12 14:27:22 +0800

    feat: 接入首页 Widget 系统组件

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { fetchConversations, createConversation, deleteConversation, fetchMessages, searchMemories, githubFile } from './utils/api'
 import { normalizeMessage } from './utils/normalize'
 import RunCard from './components/RunCard'
+import HomeWidgets, { widgets } from './components/HomeWidgets'
 import { buildSystemPrompt } from './project/instructions'
 import { getProjectMemories, addProjectMemory, deleteProjectMemory } from './project/memories'
 import Markdown from './components/Markdown'
